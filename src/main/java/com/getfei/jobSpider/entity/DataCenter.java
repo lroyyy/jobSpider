21738bdf099ca397f5d3e6eecbfbbddd84980e33
lroyyy <lroy@163.com>
2020-02-23 20:51:05 +0800

    2020-02-23 20:50

---
+++ src/main/java/com/getfei/jobSpider/entity/DataCenter.java
@@ -0,0 +1,14 @@
+package com.getfei.jobSpider.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class DataCenter extends BaseEntity{
+
+	private static final long serialVersionUID = 8430889804777983387L;
+
+	private Integer visitCount;
+	private Integer technologyCount;
+	private Integer analysisCount;
+	
+}
